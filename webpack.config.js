01aa7c54c5687348996ddb00d6d61712351f9166
Brad Gersh <bradley.gersh@gmail.com>
2021-01-29 12:04:00 -0500

    Revert "updated npm scripts"
    This reverts commit b5d661d82e304260938ba88a7800908a4f7a85a9.

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
+
 module.exports = {
-  entry: "./app/main.js",
-  mode: "development",
+  entry: './app/main.js', // assumes your entry point is the index.js in the root of your project folder
+  mode: 'development',
   output: {
-    path: __dirname,
-    filename: "public/bundle.js",
+    path: __dirname, // assumes your bundle.js will also be in the root of your project folder
+    filename: 'public/bundle.js'
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
-        },
+          loader: 'babel-loader'
+        }
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx"],
-  },
-};
+    extensions: ['*', '.js', '.jsx'],
+  }
+}
