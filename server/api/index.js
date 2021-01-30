9441a9623214591324126cd97a8b50de4e0d3b69
Vincent <vpang@law.fordham.edu>
2021-01-29 21:03:43 -0500

    T1-BE: Routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-// router.use('/users', require('./users'));
+router.use('/user', require('./user'));
 // router.use('/puppies', require('./puppies'));
 // router.use('/kittens', require('./kittens'));
 
