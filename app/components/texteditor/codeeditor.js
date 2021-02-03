ab2165d0d75b32fe2c2ca780aa81fa90b180126d
Vincent <vpang@law.fordham.edu>
2021-02-02 22:25:44 -0500

    T1-FE: Sandbox Prototype Implemented

--- app/components/texteditor/codeeditor.js
+++ app/components/texteditor/codeeditor.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios'; //temporary axios for development, will move to thunk
 
-export default function codeeditor() {
-  return (
+export default class codeeditor extends React.Component{
+
+async handleSubmit(evt){
+  evt.preventDefault()
+  console.log(evt)
+
+  const targetNode = document.getElementById('editor')
+    console.log(targetNode.innerText)
+
+    const sandboxResponse = await axios.post('/code', {code: targetNode.innerText, token: '888888888'})
+    console.log(sandboxResponse)
+    targetNode.innerText = sandboxResponse.data;
+}
+
+  render(){
+    return (
     <div>
-      <div className="codeeditor_container">
+      <form className="codeeditor_container" onSubmit={(evt)=>{this.handleSubmit(evt)}}>
         <div
           className="editor"
           id="editor"
@@ -11,9 +26,45 @@ export default function codeeditor() {
           data-placeholder="Code here!"
         ></div>
         <div className="codeeditor_button">
-          <button>Run code</button>
+          <button type="submit">Run code</button>
         </div>
-      </div>
+      </form>
     </div>
   );
+  }
 }
+
+
+// export default function codeeditor() {
+
+//   async function runcode (){
+//       const targetNode = document.getElementById('editor')
+//       // console.log(targetNode.innerText)
+
+//       const sandboxResponse = await axios.post('/code', {code: targetNode.innerText, token: '888888888'})
+
+//       targetNode.innerText = sandboxResponse;
+//   }
+
+//   function handleSubmit(evt){
+//     evt.preventDefault()
+//     console.log(evt)
+//     // runcode();
+//   }
+
+//     return (
+//       <div>
+//         <form className="codeeditor_container" onSubmit={(evt)=>{handleSubmit(evt)}}>
+//           <div
+//             className="editor"
+//             id="editor"
+//             contentEditable="true"
+//             data-placeholder="Code here!"
+//           ></div>
+//           <div className="codeeditor_button">
+//             <button type="submit">Run code</button>
+//           </div>
+//         </form>
+//       </div>
+//     );
+//   }
