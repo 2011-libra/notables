694650242e11a317ff2a47f623a1c39c3af29c32
Brad Gersh <bradley.gersh@gmail.com>
2021-02-04 12:42:14 -0500

    T2-BE: code string scanned for mismatched brackets or quotes

--- server/docker/index.js
+++ server/docker/index.js
@@ -4,21 +4,22 @@ const fs = require('fs');
 const path = require('path');
 
 const wrapCode = code => {
-  return (
-    `const { VM } = require('vm2');
-  const vm = new VM({
-    timeout: 12000,
-    sandbox: {}
-  });
+  return code;
+  // return (
+  //   `const { VM } = require('vm2');
+  // const vm = new VM({
+  //   timeout: 12000,
+  //   sandbox: {}
+  // });
 
-  const codeString = '` +
-    code +
-    `'
+  // const codeString = '` +
+  //   code +
+  //   `'
 
-  vm.run(codeString);
-  process.exit(1);
-  `
-  );
+  // vm.run(codeString);
+  // process.exit(1);
+  // `
+  // );
 };
 
 const makeWorkingDir = (token, code) => {
@@ -44,20 +45,73 @@ const cleanupWorkingDir = token => {
   }
 };
 
+// Need a second variable to keep track of whether we're inside a
+// string or not. If in string, ignore everything until string
+// closes with correct character. If string doesn't close, ->F.
+
+// A preprocessing step to make sure there are no unmatched
+// quotes or brackets
+const pairsMatch = string => {
+  const opens = {
+    '(': true,
+    '[': true,
+    '<': true,
+    '{': true
+  };
+  const closes = {
+    ')': '(',
+    ']': '[',
+    '>': '<',
+    '}': '{'
+  };
+  const stringChars = {
+    "'": true,
+    '"': true,
+    '`': true
+  };
+  const stack = [];
+  let inString = '';
+
+  for (let i = 0; i < string.length; i++) {
+    const char = string[i];
+    if (!inString) {
+      if (stringChars[char]) {
+        inString = char;
+      } else if (opens[char]) {
+        stack.push(char);
+      } else if (closes[char]) {
+        if (!stack.length) {
+          return false;
+        } else if (stack[stack.length - 1] === closes[char]) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    } else if (char === inString) {
+      inString = '';
+    }
+  }
+  return stack.length === 0 && inString === '';
+};
+
 router.post('/', async (req, res, next) => {
   const code = req.body.code;
   const token = req.body.token;
+  if (pairsMatch(code)) {
+    try {
+      makeWorkingDir(token, code);
+      const output = await sandbox(token);
+      console.log('[API Route] sandbox() output:', output);
 
-  try {
-    makeWorkingDir(token, code);
-    const output = await sandbox(token);
-    console.log('[API Route] sandbox() output:', output);
-
-    res.send(output);
-  } catch (error) {
-    next(error);
-  } finally {
-    cleanupWorkingDir(token);
+      res.send(output);
+    } catch (error) {
+      next(error);
+    } finally {
+      cleanupWorkingDir(token);
+    }
+  } else {
+    res.send('Syntax error in code');
   }
 });
 
