c6ae15e3f255849cfc3bbdce05643c0907f5be4f
Brad Gersh <bradley.gersh@gmail.com>
2021-02-12 15:42:12 -0500

    removed references to users and socket in front end

--- app/redux/index.js
+++ app/redux/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import codeEditorReducer from './CodeEditor';
 import importReducer from './import';
-import userReducer from './user';
 const appReducer = combineReducers({
   stdout: codeEditorReducer,
-  import: importReducer,
-  user: userReducer
+  import: importReducer
 });
 
 export default appReducer;
