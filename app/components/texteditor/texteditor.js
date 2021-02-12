7a2501bf5e305b0792cf5b9ec63334a2586c555f
Vincent <vpang@law.fordham.edu>
2021-02-12 11:16:30 -0500

    FIXES: Multi-Line Code Blocks Re-enabled with Shift+Enter

--- app/components/texteditor/texteditor.js
+++ app/components/texteditor/texteditor.js
@@ -44,12 +44,16 @@ function texteditor(props) {
   useEffect(() => {
     createCodeRunnerEvent();
 
-    onkeydown = (e) => {
+    onkeypress = (e) => {
       if(document.getSelection().anchorNode.parentElement.localName === 'pre' ||
       document.getSelection().anchorNode.localName === 'pre'
       ){
+        if(e.key === 'Enter' && e.shiftKey === true){
+          return;
+        }
         if(e.key === 'Enter' || e.code === 'Enter'){
           e.preventDefault()
+          alert('Use `shift + enter` to start on a new line.')
           return;
         }
       }
