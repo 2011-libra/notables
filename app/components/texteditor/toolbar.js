2c92478b4a712809d9f4fba70d6d577a46e87f7f
Vincent <vpang@law.fordham.edu>
2021-02-13 16:09:17 -0500

    CODE CLEANUP: Hyperlinking Modularized

--- app/components/texteditor/toolbar.js
+++ app/components/texteditor/toolbar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import format from '../../utils/format';
+import {setUrl, addLink} from '../../utils/hyperlink'
 import addCodeBlock from '../../utils/addCodeBlock';
 import axios from 'axios';
 import { fetchCode } from '../../redux/CodeEditor';
@@ -16,7 +17,6 @@ import './Texteditor.css';
 import { set } from 'lodash';
 
 export default function toolbar() {
-  let hyperlinkSelection = '';
 
   useEffect(() => {
     if (document.getElementById('txtFormatUrl')) {
@@ -27,67 +27,6 @@ export default function toolbar() {
       });
     }
   });
-
-  /*****************/
-  /*** HYPERLINK ***/
-  /*****************/
-  function addLink() {
-    if (
-      //***DRY CODE, FIGURE WAY TO REFACTOR LATER***//
-      document.getSelection().anchorNode === null ||
-      document.getSelection().anchorNode.innerText === '' ||
-      document.getSelection().anchorNode.className === 'toolbar' ||
-      document.getSelection().anchorNode.nodeValue === 'About us' ||
-      document.getSelection().anchorNode.nodeValue === 'Upload' ||
-      document.getSelection().anchorNode.nodeValue === 'Download' ||
-      document.getSelection().anchorNode.nodeValue === null
-      //***DRY CODE, FIGURE WAY TO REFACTOR LATER***//
-    ) {
-      alert(
-        'Please select/highlight the text you are intending to hyperlink first.'
-      );
-      return;
-    }
-
-    hyperlinkSelection = document.getSelection().anchorNode;
-    if (document.getSelection().anchorNode.parentElement.localName === 'pre') {
-      alert('You can not add a hyperlink inside a code block');
-      return;
-    }
-
-    const hypNode = document.getElementById('url-input');
-    if (hypNode.classList.contains('hidden')) {
-      hypNode.classList.remove('hidden');
-    } else {
-      hypNode.classList.add('hidden');
-    }
-  }
-
-  /*******************/
-  /*** SETTING URL ***/
-  /*******************/
-  function setUrl(e) {
-    e.preventDefault();
-    if (document.getSelection().anchorNode.parentElement.localName === 'pre') {
-      return;
-    }
-
-    document.getElementById('url-input').className = 'hidden';
-    const url = document.getElementById('txtFormatUrl').value;
-    if (url === '') {
-      return;
-    }
-    let currSelection = hyperlinkSelection;
-    let currStr = currSelection.data;
-    let newHyperlink = document.createElement('a');
-    newHyperlink.innerText = currStr;
-    newHyperlink.href = `https://${url}`;
-    newHyperlink.target = '_blank';
-    newHyperlink.contentEditable = false;
-
-    currSelection.parentNode.insertBefore(newHyperlink, currSelection);
-    currSelection.parentNode.removeChild(currSelection);
-  }
 
   return (
     <div className="toolbar">
