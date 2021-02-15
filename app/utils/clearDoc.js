ddbfdd9982b9afe49aff13f50e6f7cebd1d99963
Matthew Leng <mleng89@gmail.com>
2021-02-14 19:35:35 -0500

    T2-FE: Delete button gives pop up instead of alert

--- app/utils/clearDoc.js
+++ app/utils/clearDoc.js
@@ -1,5 +1,61 @@
-export default function clearDoc (){
-  if(confirm('⚠️ WARNING ⚠️ : Are you sure you want to DELETE this document?')){
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { BsTrash } from 'react-icons/bs';
+export default function clearDoc() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleConfirm = () => {
+    setOpen(false);
+
     document.getElementById('contentEditable').innerHTML = '';
-  }
+  };
+
+  return (
+    <div>
+      <Button title="Delete Document" id="trash-doc" onClick={handleClickOpen}>
+        <BsTrash />
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{'⚠️ WARNING ⚠️ '}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to DELETE this document?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            No
+          </Button>
+          <Button onClick={handleConfirm} color="primary" autoFocus>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
+
+// export default function clearDoc() {
+//   if (
+//     confirm('⚠️ WARNING ⚠️ : Are you sure you want to DELETE this document?')
+//   ) {
+//     document.getElementById('contentEditable').innerHTML = '';
+//   }
+// }
